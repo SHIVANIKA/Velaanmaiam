cee10e8d75bfb42b3eb6b714a00752aa9874114a
MyCode-Park <90173198+MyCode-Park@users.noreply.github.com>
2021-09-11 10:39:11 +0530

    Base codes are updated (This code can be used to start working on the website)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         MY Codes are published in the GIT HUB Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>
+         Velaanmaiam
+       </h1>
       </header>
     </div>
   );
--- my-app/src/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-This is Ammend that will replace the previous commit and now I am gonna pull this in the vs code.
-
-This time I am gonna push it to the Repo.
-
-This is a merge request from Vicky-space okay
