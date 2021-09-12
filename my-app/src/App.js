64800e295cc57d738f23aa1b442aa99250bfb8cd
Shivanika <shivanika12@gmail.com>
2021-09-12 11:43:23 +0530

    Home bg with text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,43 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Button, container, Figure } from "react-bootstrap"
+import agri from './agriculture-bg.jpg';
+import FigureCaption from "react-bootstrap/esm/FigureCaption";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <h1>
-         Velaanmaiam 
-       </h1>
-      
-      </header>
+
+export default function Homeblog(){
+  return <div>
+    <container>
+      <Figure className="position-relative">
+      <img src = {agri} alt="agri" className ="img-fluid"/>
+        <FigureCaption className = 'blogText'>
+          <h3>Who we are </h3>
+         <p>We VELAANMAIAM is bridge between you </p>
+           <p> our buyers and sellers to make the organic </p>
+          <p> products purchase easy and efficient way. </p>
+          <div className="button">
+         <Button onClick="know more">Knowmore</Button>
+         </div>
+        </FigureCaption>
+        </Figure>
+      </container>
     </div>
-  );
+
 }
 
-export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//        <h1>
+//          Velaanmaiam 
+//        </h1>
+      
+//       </header>
+//     </div>
+//   );
+// }
+
